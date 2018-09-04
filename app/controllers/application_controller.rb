c919ce605750400cc128184f6b2ed623ceca173c
Jason L Perry <jason@suncoast.io>
2018-09-04 11:08:43 -0400

    Fix session bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue StandardError
+    session[:user_id] = nil
   end
   helper_method :current_user
 
   def signed_in?
-    current_user.nil?
+    !current_user.nil?
   end
   helper_method :signed_in?
 end
