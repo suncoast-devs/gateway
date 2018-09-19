7f4003f34ebe78cfa82b50f86b7f57b78006a575
Jason L Perry <jason@suncoast.io>
2018-09-18 22:12:47 -0400

    Production configuration

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
+
+preload_app!
 
 port        ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
-plugin :tmp_restart
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
