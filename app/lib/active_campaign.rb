8286d485dee9ef555162380455f7e10866471366
Jason L Perry <jason@suncoast.io>
2019-01-15 10:58:49 -0500

    Update
    A lazy commit.

--- app/lib/active_campaign.rb
+++ app/lib/active_campaign.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-BASE_URL = Rails.application.credentials.active_campaign_url
-API_KEY = Rails.application.credentials.active_campaign_key
-
 module ActiveCampaign
+  BASE_URL = Rails.application.credentials.active_campaign_url
+  API_KEY = Rails.application.credentials.active_campaign_key
+
   def self.get(action, params = nil)
     HTTP.headers("Api-Token" => API_KEY).get("#{BASE_URL}/#{action}", params: params).parse
   end
--- app/lib/short_url.rb
+++ app/lib/short_url.rb
@@ -1,10 +1,10 @@
-API_KEY = Rails.application.credentials.sdg_codes_api_key
-API_URL = {
-  "development" => "http://0.0.0.0:3001",
-  "production" => "https://sdg.codes",
-}
-
 module ShortURL
+  API_KEY = Rails.application.credentials.sdg_codes_api_key
+  API_URL = {
+    "development" => "http://0.0.0.0:3001",
+    "production" => "https://sdg.codes",
+  }
+
   def self.generate(label, url, slug = nil)
     data = {label: label, url: url, slug: slug}
     HTTP.headers("Authorization" => "Bearer #{API_KEY}")
