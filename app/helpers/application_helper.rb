6b56f77fc768f8c81b663ac344cf1ac4b710db2f
Jason L Perry <jason@suncoast.io>
2019-01-17 11:41:45 -0500

    Page titles and AC tracking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,36 @@
 # frozen_string_literal: true
 
-require 'pagy/extras/bulma'
+require "pagy/extras/bulma"
 
 # :nodoc:
 module ApplicationHelper
   include Pagy::Frontend
 
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
   def gravatar_for(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def format_answer(answer)
     case answer
-    when true then 'Yes'
-    when false then 'No'
+    when true then "Yes"
+    when false then "No"
     else
       simple_format answer.to_s
     end
   end
 
   def current_controller?(test_name)
-    'is-active' if controller.controller_name == test_name
+    "is-active" if controller.controller_name == test_name
   end
 
   def usd(amount)
-    number_to_currency amount, unit: '$'
+    number_to_currency amount, unit: "$"
   end
 
   def markdown(markdown)
