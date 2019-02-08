ac7e54ba1b873f4f3bd1657b5e17a6eac00b3883
Jason L Perry <jason@suncoast.io>
2019-02-08 13:56:21 -0500

    Use merge

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -16,6 +16,15 @@ module Taggable
     end
   end
 
+  def merge_tags(new_names)
+    tag_names = tags.map(&:name)
+    new_names = names.split(",")
+
+    self.tags = (tag_names + new_names).uniq.map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
   module ClassMethods
     def tag_counts
       Tag.select("tags.*, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
