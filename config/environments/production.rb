5479aa49742a94908db01c4a1f215f3c5b1d52b3
Jason L Perry <jason@suncoast.io>
2019-04-14 10:58:57 -0400

    Update
    A lazy commit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.force_ssl = true
+  config.assets.compile = false
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new(STDOUT)
