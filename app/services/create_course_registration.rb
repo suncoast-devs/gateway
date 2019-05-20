2ffd8f14818bc4f3633320cab166933498fd385f
Jason L Perry <jason@suncoast.io>
2019-05-20 15:39:29 -0400

    Fix registrations

--- app/services/create_course_registration.rb
+++ app/services/create_course_registration.rb
@@ -5,7 +5,7 @@ class CreateCourseRegistration
 
   def initialize(person_id, identifier, code)
     @person = Person.find(person_id)
-    @course = Course.where(identifier: identifier).first
+    @course = Course.where(identifier: identifier).where("starts_on > ?", Date.today).first
     @identifier = identifier
     @code = code
   end
@@ -16,7 +16,7 @@ class CreateCourseRegistration
     due_date = [@course.starts_on - 7, 1.day.from_now].max
     invoice = @person.invoices.create(due_on: due_date,
                                       invoice_items_attributes: [
-                                        {description: "#{@course.name} (#{@course.session})", quantity: 1, amount: registration.fee},
+                                        { description: "#{@course.name} (#{@course.session})", quantity: 1, amount: registration.fee },
                                       ])
     registration.update(invoice: invoice)
     CreateInvoice.call(invoice.id)
