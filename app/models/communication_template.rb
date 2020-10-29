3f20ee21bef1e3c6ab185a8f5f3aec3f66de1d26
Jason L Perry <jason@suncoast.io>
2020-10-29 10:51:43 -0400

    Extract template renderer to service.

--- app/models/communication_template.rb
+++ app/models/communication_template.rb
@@ -7,34 +7,11 @@ class CommunicationTemplate < ApplicationRecord
   validates :key, presence: true, if: :is_system?
 
   def render_title(person)
-    template = Liquid::Template.parse(title)
-    template.render(params(person))
+    PersonTemplateRenderer.new(person, title).render
   end
 
   def render_body(person)
-    template = Liquid::Template.parse(body)
-    rendered_body = template.render(params(person))
-    
-    if sms?
-      rendered_body
-    else
-      document = CommonMarker.render_doc(rendered_body, [:DEFAULT, :FOOTNOTES])
-      document.to_html(:DEFAULT).html_safe
-    end
-  end
-
-  # person, enrollment, cohort, acceptance
-  def params(person)
-    return @params if @params
-    program_enrollment = person.current_program_enrollment
-    program_acceptance = person.current_program_acceptance
-    cohort = program_enrollment&.cohort
-    @params = {
-      "person" => person,
-      "cohort" => cohort,
-      "enrollment" => program_enrollment,
-      "acceptance" => program_acceptance
-    }
+    PersonTemplateRenderer.new(person, body, markdown: !sms?).render    
   end
 
   # TODO: Handle SMS.
