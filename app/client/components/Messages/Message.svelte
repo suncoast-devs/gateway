9e029779f4c3862f2e3a38fed595a2e620b26b0b
Jason L Perry <jason@suncoast.io>
2020-10-31 20:37:57 -0400

    Fix own styles

--- app/client/components/Messages/Message.svelte
+++ app/client/components/Messages/Message.svelte
@@ -5,14 +5,14 @@
   export let subject = null
   export let body
   export let media
-  export let isOwn = false
+  export let isOutgoing = false
 
   const smsStyle = 'bg-green-100 border-green-200 text-green-900'
   const emailStyle = 'bg-blue-100 border-blue-200 text-blue-900'
-  const isOwnStyle = 'justify-self-end flex-row-reverse'
+  const isOwnStyle = 'justify-self-end flex-row-reverse opacity-75'
 </script>
 
-<li class={`mx-2 my-1 flex items-center ${isOwn && isOwnStyle}`}>
+<li class={`mx-2 my-1 flex items-center ${isOutgoing && isOwnStyle}`}>
   <div
     class={`px-3 py-2 w-8/12 border overflow-x-hidden rounded-md ${media === 'sms' ? smsStyle : emailStyle}`}>
     {#if subject}
