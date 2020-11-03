c4307769bc436bab18e2c83ad47a515aa9fb617f
Jason L Perry <jason@suncoast.io>
2020-11-03 12:47:10 -0500

    Safer handling of start question

--- app/services/create_program_enrollment.rb
+++ app/services/create_program_enrollment.rb
@@ -14,18 +14,26 @@ class CreateProgramEnrollment
       if @program_application.person.program_enrollments.count > 0
         @program_application.update(program_enrollment: @program_application.person.current_program_enrollment)
       else
-        program_start = @program_application.question_responses["When are you hoping to start the program?"]
-        cohort_name = program_start.match(/(\d+)/).try(:[], 1) || "Future"
         enrollment = ProgramEnrollment.create!({
-          cohort: Cohort.where(name: cohort_name).first,
+          cohort: Cohort.where(name: program_start).first,
           person: @program_application.person,
           program: "web-development",
           program_applications: [@program_application],
         })
 
-        CommunicationTemplate.by_key('application-received')&.send_to @program_application.person
+        CommunicationTemplate.by_key("application-received")&.send_to @program_application.person
         InterviewReminder.call_in 2.days, @program_application.person
       end
     end
+  end
+
+  private
+
+  def program_start
+    keys = @program_application.question_responses.keys.grep /start the program/
+    answer = @program_application.question_responses[keys.first]
+    answer.match(/(\d+)/).try(:[], 1) || "Future"
+  rescue
+    "Future"
   end
 end
