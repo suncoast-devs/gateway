da5d064a28d709fe500b18e6d7f03767a684b0b6
Jason L Perry <jason@suncoast.io>
2020-11-13 22:52:14 -0500

    Don't clobber existing full name

--- app/models/person.rb
+++ app/models/person.rb
@@ -41,6 +41,6 @@ class Person < ApplicationRecord
   private
 
   def update_full_name
-    self.full_name = [given_name, middle_name, family_name].join(" ").squeeze(" ")
+    self.full_name ||= [given_name, middle_name, family_name].join(" ").squeeze(" ")
   end
 end
