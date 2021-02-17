ae4183b03dac8af81488e71c9b1c6934d5d43fa1
Jason L Perry <jason@suncoast.io>
2021-02-17 10:46:41 -0500

    Captue POST data in exceptions

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,5 @@
 Raven.configure do |config|
   config.dsn = Rails.application.credentials.raven_dsn
   config.environments = %w(staging production)
+  config.processors -= [Raven::Processor::PostData]
 end
