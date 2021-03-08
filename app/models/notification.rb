f7208a383e782483429a971d063b83b0eaeb4dda
Jason L Perry <jason@suncoast.io>
2021-03-08 11:39:47 -0500

    Update notification.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,20 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  after_create :broadcast
-
-  def broadcast
-    NotificationChannel.broadcast_to user, {
-      id: id,
-      event: {
-        name: event.name,
-        title: event.title,
-        message: event.message,
-        link: event.link_url,
-      },
-    }
-  end
-
   def acknowledge!
     update acknowledged_at: Time.now
   end
