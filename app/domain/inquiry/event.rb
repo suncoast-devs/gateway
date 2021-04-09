aaec65560544bd451eb2c2d3f96d2fb13ecb8fa7
Jason L Perry <jason@suncoast.io>
2021-04-09 01:19:00 -0400

    Update event.rb

--- app/domain/inquiry/event.rb
+++ app/domain/inquiry/event.rb
@@ -1,4 +1,9 @@
+# NOTE: Can maybe use advisory locks (pg_advisory_xact_lock) to ensure linearized event captures
 class Inquiry::Event < ActiveRecord::Base
   scope :by_aggregate, ->(root_id) { where(aggregate: root_id).order(id: "ASC") }
   serialize :payload, JsonHashSerializer
+
+  def readonly?
+    !new_record?
+  end
 end
