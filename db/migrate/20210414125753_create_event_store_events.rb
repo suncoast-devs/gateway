d921ac716a25c56d1baceb40d255608de2c35d40
Jason L Perry <jason@suncoast.io>
2021-04-14 09:27:02 -0400

    Hello, Rails Event Store

---
+++ db/migrate/20210414125753_create_event_store_events.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class CreateEventStoreEvents < ActiveRecord::Migration[4.2]
+  def change
+    create_table(:event_store_events_in_streams, force: false) do |t|
+      t.string :stream, null: false
+      t.integer :position, null: true
+      t.references :event, null: false, type: :uuid
+      t.datetime :created_at, null: false
+    end
+
+    add_index :event_store_events_in_streams, [:stream, :position], unique: true
+    add_index :event_store_events_in_streams, [:created_at]
+    add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
+
+    create_table(:event_store_events, force: false) do |t|
+      t.references :event, null: false, type: :uuid
+      t.string :event_type, null: false
+      t.binary :metadata
+      t.binary :data, null: false
+      t.datetime :created_at, null: false
+      t.datetime :valid_at, null: true
+    end
+
+    add_index :event_store_events, :event_id, unique: true
+    add_index :event_store_events, :created_at
+    add_index :event_store_events, :valid_at
+    add_index :event_store_events, :event_type
+  end
+end
