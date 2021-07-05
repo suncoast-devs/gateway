e7bf72efcc0078658c60e7be36c904fed7eb23df
Jason L Perry <jason@suncoast.io>
2021-07-05 12:43:58 -0400

    Update Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,4 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.raven_dsn
   config.send_default_pii = true
-  config.environments = %w[staging production]
-  config.processors -= [Raven::Processor::PostData]
 end
