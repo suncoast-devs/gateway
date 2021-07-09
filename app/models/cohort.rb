8dceb544302893f34aeaa38ead2243e16ad8240f
Jason L Perry <jason@suncoast.io>
2021-07-09 17:42:56 -0400

    Browsable Cohort Future

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -7,7 +7,11 @@ class Cohort < ApplicationRecord
   end
 
   def alt_display_name
-    "#{name} (#{begins_on ? begins_on.strftime("%B %Y") : "TBD"})"
+    if name == "Future"
+      display_name
+    else
+      "#{name} (#{begins_on ? begins_on.strftime("%B %Y") : "TBD"})"
+    end
   end
 
   def tuition_due_date
@@ -17,5 +21,9 @@ class Cohort < ApplicationRecord
 
   def to_liquid
     CohortDrop.new(self)
+  end
+
+  def self.enrolling_and_future
+    enrolling.or(where(name: 'Future'))
   end
 end
