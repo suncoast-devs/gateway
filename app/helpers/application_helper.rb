e0b150fcec4543390af541de86651284366900ee
Jason L Perry <jason@suncoast.io>
2021-07-09 13:43:43 -0400

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
   end
 
   def gravatar_for(user)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user&.email&.downcase || "?")
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
