1a89022076d8030e8f33b24827cfc70bc687786f
Jason L Perry <jason@suncoast.io>
2021-07-09 21:19:27 -0400

    Update cohort_spec.rb

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Cohort, type: :model do
-  subject { described_class.new(name: '42') }
+  subject(:cohort) { described_class.new(name: '42') }
 
   it 'formats a display name' do
-    expect(subject.display_name).to eq('Cohort 42')
+    expect(cohort.display_name).to eq('Cohort 42')
   end
 end
