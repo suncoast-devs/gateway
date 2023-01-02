19dbb6c31ee3e611823ac093fb91c87cb3f66869
Jason L Perry <298+ambethia@users.noreply.github.com>
2023-01-02 05:09:57 -0500

    fixup! Add payment URLS

--- app/drops/program_acceptance_drop.rb
+++ app/drops/program_acceptance_drop.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ProgramAcceptanceDrop < Liquid::Drop
-  delegate :enrollment_agreement_url, :note, :is_update, to: :@program_acceptance, allow_nil: true
+  delegate :enrollment_agreement_url, :note, :is_update, :payment_url, to: :@program_acceptance, allow_nil: true
 
   def initialize(program_acceptance)
     @program_acceptance = program_acceptance
